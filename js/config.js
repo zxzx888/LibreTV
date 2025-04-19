7b95703a731ca0dd451857622d609b2c821341c6
zxzx888 <zxzx888@gmail.com>
2025-04-19 08:46:07 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // 全局常量配置
-const PROXY_URL = 'https://down.nigx.cn/';    // 适用于 Cloudflare, Netlify (带重写), Vercel (带重写)
+const PROXY_URL = 'https://seep.eu.org/';    // 适用于 Cloudflare, Netlify (带重写), Vercel (带重写)
 const HOPLAYER_URL = 'https://hoplayer.com/index.html';
 const SEARCH_HISTORY_KEY = 'videoSearchHistory';
 const MAX_HISTORY_ITEMS = 5;
